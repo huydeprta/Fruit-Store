0129fc8175c2160dd88703f0cb1922fbf8941329
huydeprta <caothaihuy14@gmail.com>
2024-07-17 20:31:55 +0700

    up

--- Client/src/pages/Admin/Admin.jsx
+++ Client/src/pages/Admin/Admin.jsx
@@ -7,9 +7,25 @@ const Admin = () => {
             <div>
                 <div>
                     <nav>
-                         <h1>cc</h1>
+                       <div>
+                       <span className="material-symbols-outlined">menu</span>
+                        <h2>Hệ Thống Quản Lý</h2>
+                        </div>  
+                        <div>
+                        <span className="material-symbols-outlined">search</span>
+                        </div>
                     </nav>
                 </div>
+                <div>
+                        <div>
+                            <ul>
+                                <li>
+                                    <h3>DANH MỤC QUẢN LÝ</h3>
+                                </li>
+                                <li>Quản Lý Sản phẩm</li>
+                            </ul>
+                        </div>
+                    </div>
             </div>
      
         </>
--- Client/src/pages/Product.jsx
+++ Client/src/pages/Product.jsx
@@ -4,52 +4,64 @@ import Footer from "../layout/Footer/Footer";
 const Products = () => {
   return (
     <>
-    <Navbar/>
-    <div>
-    
-    </div>
-    <div>
+      <Navbar />
       <div>
-        <div className="none">
+          <div >
+            <aside>
+              <span>DANH MỤC SẢN PHẨM</span>
+              <ul>
+                <li className="cart-item"><a>Trái cây ngon hôm nay</a></li>
+                <li className="cart-item"><a>Trái cây việt nam</a></li>
+                <li className="cart-item"><a>Trái cây nhập khẩu</a></li>
+                <li className="cart-item"><a>Trái cây cắt sẵn</a></li>
+                <li className="cart-item"><a>Trái cây sấy khô</a></li>
+                <li className="cart-item"><a>Nước ép trái cây nguyên chất</a></li>
+              </ul>
+            </aside>
+          </div>
+      </div>
+      <div>
+        <div>
+          <div className="none">
             <div className="product-card">
-                <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
-                <p>Du du ruot vang huu co</p>
-                <p className="price">65,000₫</p>
-                <button className="buy-button">CHỌN MUA</button>
+              <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
+              <p>Du du ruot vang huu co</p>
+              <p className="price">65,000₫</p>
+              <button className="buy-button">CHỌN MUA</button>
             </div>
             <div className="product-card">
-                <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
-                <p>Du du ruot vang huu co</p>
-                <p className="price">65,000₫</p>
-                <button className="buy-button">CHỌN MUA</button>
+              <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
+              <p>Du du ruot vang huu co</p>
+              <p className="price">65,000₫</p>
+              <button className="buy-button">CHỌN MUA</button>
             </div>
             <div className="product-card">
-                <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
-                <p>Du du ruot vang huu co</p>
-                <p className="price">65,000₫</p>
-                <button className="buy-button">CHỌN MUA</button>
+              <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
+              <p>Du du ruot vang huu co</p>
+              <p className="price">65,000₫</p>
+              <button className="buy-button">CHỌN MUA</button>
             </div>
             <div className="product-card">
-                <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
-                <p>Du du ruot vang huu co</p>
-                <p className="price">65,000₫</p>
-                <button className="buy-button">CHỌN MUA</button>
+              <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
+              <p>Du du ruot vang huu co</p>
+              <p className="price">65,000₫</p>
+              <button className="buy-button">CHỌN MUA</button>
             </div>
             <div className="product-card">
-                <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
-                <p>Du du ruot vang huu co</p>
-                <p className="price">65,000₫</p>
-                <button className="buy-button">CHỌN MUA</button>
+              <img src="https://product.hstatic.net/200000377165/product/artboard_6_6b979af74f1447afb0e256d58b80611a_large.png" alt="Papaya" className="product-image" />
+              <p>Du du ruot vang huu co</p>
+              <p className="price">65,000₫</p>
+              <button className="buy-button">CHỌN MUA</button>
             </div>
-          
-         
-          
+
+
+
+          </div>
         </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
-    
+
   );
 };
 
