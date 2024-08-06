fd9d828fd484991e79b620e3965fa82021afbe2e
van Thiết <tretrauthiet@gmail.com>
2024-08-06 21:25:43 +0700

    update

--- Client/src/pages/Admin/content/ManangerClient.jsx
+++ Client/src/pages/Admin/content/ManangerClient.jsx
@@ -3,29 +3,32 @@ import AuthService from "../../../services/authService";
 import { format } from 'date-fns';
 import DeleteButton_square from "../../../components/Button/DeleteButton";
 import CheckButton from "../../../components/Button/CheckButton";
+
 const ManagerClient = () => {
-    const [allUser,setAllUser] = useState([])
-    const getAlluser = async ()=>{
-         const User = await AuthService.GetallUser()
-         console.log(User);
-         
-         setAllUser(User);
-    }
-    useEffect(()=>{
-        getAlluser()
-    },[])
+    const [allUser, setAllUser] = useState([]);
+    
+    const getAlluser = async () => {
+        const User = await AuthService.GetallUser();
+        console.log(User);
+        setAllUser(User);
+    };
+    
+    useEffect(() => {
+        getAlluser();
+    }, []);
+    
     console.log(allUser);
     
     return (
         <>
             <div className="flex-1  undefine ">
-                <table className=" w-full ">
+                <table className="w-full">
                     <thead className="bg-[#FFFFFF] h-[50px]">
-                        <tr >
+                        <tr>
                             <th></th>
                             <th className="text-left w-[150px]">Tên</th>
                             <th className="text-left w-[150px]">Admin</th>
-                            <th className="text-left w-[150px]" >Trạng thái</th>
+                            <th className="text-left w-[150px]">Trạng thái</th>
                             <th className="text-left w-[150px] pl-[50px]">Ngày tạo</th>
                             <th></th>
                         </tr>
@@ -48,25 +51,29 @@ const ManagerClient = () => {
                                         </span>)}</td>
 
                                 <td data-label="Created" className="lg:w-1 whitespace-nowrap pl-[50px]">
-                                    <small className="text-gray-500 dark:text-slate-400"> {user.createdAt && format(new Date(user.createdAt), 'yyyy-MM-dd HH:mm:ss')}</small>
+                                    <small className="text-gray-500 dark:text-slate-400">{user.createdAt && format(new Date(user.createdAt), 'yyyy-MM-dd HH:mm:ss')}</small>
                                 </td>
                                 <td className="before:hidden lg:w-1 whitespace-nowrap">
                                     <div className="flex items-center justify-start lg:justify-end undefined -mb-3 flex-nowrap">
-                                        <CheckButton  />
-                                        {/* clickCheck={
-                                            () => {
-                                                getDetailUser(user._id)
-                                                setShowDetailUser(true)
-                                            }} */}
-                                        <DeleteButton_square  />
-                                        {/* clickDelete={() => { handleDelete(user._id) }} */}
+                                        {!user.admin && (
+                                            <>
+                                                <CheckButton 
+                                                    // clickCheck={() => {
+                                                    //     getDetailUser(user._id);
+                                                    //     setShowDetailUser(true);
+                                                    // }}
+                                                />
+                                                <DeleteButton_square 
+                                                    // clickDelete={() => { handleDelete(user._id); }}
+                                                />
+                                            </>
+                                        )}
                                     </div>
                                 </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
-
             </div>
         </>
     );
